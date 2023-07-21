196baaf4285b106f9e20291391c13e57d3adcfd8
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-07-21 17:45:36 +0900

    up

--- _sb/230721_1624.py
+++ _sb/230721_1624.py
@@ -9,13 +9,14 @@ class MyScene(Scene):
 
 if __name__ == '__main__':
 
-  orientation = DEFAULT_ORIENTATION
+  orientation = PORTRAIT
   frame_interval = 1
-  anti_alias = False
-  show_fps = False
-  multi_touch = True
+  show_fps = True
 
   my_scene = MyScene()
 
-  run(my_scene)
+  run(my_scene,
+      orientation=orientation,
+      frame_interval=frame_interval,
+      show_fps=show_fps)
 
