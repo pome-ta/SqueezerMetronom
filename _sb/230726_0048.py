a5703b34a2fd55aad7c1763bcb9a7aa2e7b2b2a1
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-07-26 15:20:50 +0900

    up

--- _sb/230726_0048.py
+++ _sb/230726_0048.py
@@ -10,29 +10,48 @@ class Canvas(scene.Scene):
   def setup(self):
     print('setup')
     print(f'{self.size=}')
+    self.ground = scene.Node(parent=self)
+    self.set_line(128)
 
   def update(self):
     pass
-    
+
   def did_evaluate_actions(self):
     pass
 
   def did_change_size(self):
     print('did_change_size')
     print(f'{self.size=}')
+    #self.set_line(128)
+
+  def set_line(self, dire):
+    w2, h2 = self.size / 2
+    path = ui.Path()
+    path.move_to(w2 - dire, h2 - dire)
+    path.line_to(w2 + dire, h2 + dire)
+    line = scene.ShapeNode(parent=self.ground)
+    line.path = path
+    line.stroke_color = 'red'
+    line.position = self.size / 2
 
 
 class View(ui.View):
 
   def __init__(self, canvas):
-    self.bg_color = 1
+    self.bg_color = 0.88
+    self.height_ratio: float = 0.96  # todo: safe area
+
     self.canvas = canvas
     self.scene_view = scene.SceneView(scene=self.canvas)
-    self.scene_view.flex = 'W'
+
+    #self.scene_view.flex = 'WH'
     self.add_subview(self.scene_view)
-    
+
   def layout(self):
-    pass
+    _, _, w, h = self.frame
+    self.scene_view.width = w
+    self.scene_view.height = h * self.height_ratio
+    self.scene_view.x = (w / 2) - (self.scene_view.width / 2)
 
 
 if __name__ == '__main__':
