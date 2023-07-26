8b5c02f7c7633e9184c35b64a41cf1ca6debdd1e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-07-26 11:53:04 +0900

    up

--- _sb/230726_0048.py
+++ _sb/230726_0048.py
@@ -4,26 +4,42 @@ import ui
 frame_interval = 1
 shows_fps = True
 
-'''
-class SceneCanvas(scene.SceneView):
 
-  def __init__(self):
+class Canvas(scene.Scene):
+
+  def setup(self):
+    print('setup')
+    print(f'{self.size=}')
+
+  def update(self):
     pass
-'''
+    
+  def did_evaluate_actions(self):
+    pass
+
+  def did_change_size(self):
+    print('did_change_size')
+    print(f'{self.size=}')
+
 
 class View(ui.View):
 
   def __init__(self, canvas):
     self.bg_color = 1
     self.canvas = canvas
-    self.add_subview(self.canvas)
+    self.scene_view = scene.SceneView(scene=self.canvas)
+    self.scene_view.flex = 'W'
+    self.add_subview(self.scene_view)
+    
+  def layout(self):
+    pass
 
 
 if __name__ == '__main__':
   TITLE = 'title'
 
   #scene_canvas = SceneCanvas()
-  scene_canvas = scene.SceneView()
+  scene_canvas = Canvas()
   view = View(scene_canvas)
   #view = scene.SceneView()
   view.present(style='fullscreen', orientations=['portrait'])
