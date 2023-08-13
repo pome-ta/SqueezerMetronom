b7396b579e1022fbd885323fc51936526b010a54
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-08-13 16:40:36 +0900

    up

--- _sb/230813_1516.py
+++ _sb/230813_1516.py
@@ -5,6 +5,7 @@ from objc_util import ObjCClass, uiimage_to_png
 import pdbg
 
 UIImage = ObjCClass('UIImage')
+UIColor = ObjCClass('UIColor')
 UIImageSymbolConfiguration = ObjCClass('UIImageSymbolConfiguration')
 
 BEAT: int = 4
@@ -12,7 +13,7 @@ BEAT: int = 4
 frame_interval: int = 1
 shows_fps: bool = True
 
-symbol_play = 'play'
+symbol_play = 'play.fill'
 symbol_stop = 'stop'
 
 
@@ -26,44 +27,36 @@ def configuration_by_applying_configuration(
 
 def get_symbo_icon(symbol_name: str, point_size: float = 128.0) -> ui.Image:
 
-  _point_size = UIImageSymbolConfiguration.configurationWithPointSize_(
-    point_size)
-  _multicolor = UIImageSymbolConfiguration.configurationPreferringMulticolor()
+  size = UIImageSymbolConfiguration.configurationWithPointSize_(point_size)
+  #color = UIImageSymbolConfiguration.configurationPreferringMulticolor()
+  color = UIImageSymbolConfiguration.configurationPreferringMonochrome()
 
-  conf = configuration_by_applying_configuration([_point_size, _multicolor])
+  conf = configuration_by_applying_configuration([size, color])
+  #conf = configuration_by_applying_configuration([size])
 
-  ui_image = UIImage.systemImageNamed_withConfiguration_(symbol_name, conf)
+  tint_color = UIColor.systemDarkGrayColor()
+  tint_color = UIColor.tintColor()
+  
+  '''
+  case automatic = 0
+  case alwaysOriginal = 1
+  case alwaysTemplate = 2
+  '''
+
+  ui_image = UIImage.systemImageNamed_withConfiguration_(
+    symbol_name, conf).imageWithTintColor_renderingMode_(tint_color, 1)
+  #imageWithTintColor_renderingMode_
+  
+  #pdbg.state(UIColor)
+  pdbg.state(ui_image)
+  #ui_image.imageWithTintColor_renderingMode_(UIColor.systemDarkGrayColor(), 1)
+
+  #pdbg.state(ui_image)
+  #pdbg.state(UIColor.systemDarkGrayColor())
 
   png_bytes = uiimage_to_png(ui_image)
   png_img = ui.Image.from_data(png_bytes, 2)
-
-  #pdbg.state(ui_image)
-  to_png = uiimage_to_png(ui_image)
-  #print(to_png)
-
-  #re_img = ui.Image.from_data(to_png)
-  #print(re_img)
-  pil_img = ImageP.open(BytesIO(to_png))
-  #pil_img.thumbnail((128,128))
-  img_bytes = BytesIO()
-  pil_img.save(img_bytes, format='PNG')
-  img_bytes = img_bytes.getvalue()
-  #png_img = ui.Image.from_data(img_bytes, 2)
-  png_img = ui.Image.from_data(to_png, 2)
-  #newimg.
-
-  #iii = ImageP.frombytes('RGBA', (20, 20), to_png)
-
-  #png_img = ui.Image.from_data(iii, 2)
-  #png_img = ui.Image.from_data(to_png, 2)
   return png_img
-  '''
-  with BytesIO() as bIO:
-    iii.save(bIO, 'png')
-    re_img = ui.Image.from_data(bIO.getvalue())
-    #del iii
-    return re_img
-  '''
 
 
 class Canvas(scene.Scene):
@@ -72,11 +65,10 @@ class Canvas(scene.Scene):
     super().__init__(*args, **kwargs)
     self.icon_wrap: scene.ShapeNode
     self.icon_sprite: scene.SpriteNode
-    #self.aaa = get_symbo_icon('cloud.sun.rain.fill')
-    '''
+
     _png_img = get_symbo_icon(symbol_play)
-    '''
-    _png_img = get_symbo_icon('cloud.sun.rain.fill')
+
+    #_png_img = get_symbo_icon('cloud.sun.rain.fill')
     self.play_tex = scene.Texture(_png_img)
 
   def setup(self):
@@ -107,6 +99,7 @@ class Canvas(scene.Scene):
 
     self.icon_sprite.texture = self.play_tex
     self.icon_sprite.size = (o_size * 0.64, o_size * 0.64)
+    #self.icon_sprite.color = 'cyan'
 
   def __init_guide(self):
     self.guide = scene.ShapeNode(parent=self,
